de3411ab6bd9242d9cafa81df91149a84bd59575
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-06-11 09:14:55 +0530

    a small learning activity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-
+import "./msgBox.jsx";
+import MsgBox from "./msgBox.jsx";
 function Title() {
   return <h1>I am <span>Title</span></h1>;
 }
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Title />
       <button>Hello World !</button>
+      <MsgBox userName="shubhi" textColor="yellow"/>
     </div>
   );
 }
