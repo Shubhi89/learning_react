53aeb60715f3445a402521da0965b66441c47408
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-06-15 10:55:03 +0530

    adding mark as done feature

--- src/todoList.jsx
+++ src/todoList.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList() {
-    let [todos, setTodos] = useState([{task : "sample-task" , id: uuidv4()}]);
+    let [todos, setTodos] = useState([{task : "sample-task" , id: uuidv4() , isDone: false}]);
     let [newTodo , SetNewTodo] = useState("");
 
     let addNewTask = () => {
         setTodos((prevTodos) => {
-            return([...prevTodos , {task: newTodo , id: uuidv4()}]);
+            return([...prevTodos , {task: newTodo , id: uuidv4() , isDone: false}]);
         });
         SetNewTodo("");
     };
@@ -31,12 +31,12 @@ export default function TodoList() {
         );
     };
 
-    let upperCaseOne = (id) => {
+    let markAsDone = (id) => {
         setTodos((prevTodos) => 
             prevTodos.map((todo) => {
                 if(todo.id == id) {
                     return {
-                        ...todo , task:todo.task.toUpperCase(),
+                        ...todo , isDone : true,
                     };
                 } else {
                     return todo;
@@ -58,10 +58,10 @@ export default function TodoList() {
                     todos.map((todo) => {
                         return (
                         <li key={todo.id}>
-                            <span>{todo.task}</span>
+                            <span style={todo.isDone?{textDecorationLine : "line-through"}: {}}>{todo.task}</span>
                             &nbsp;&nbsp;&nbsp;
                             <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-                            <button onClick={() => upperCaseOne(todo.id)}>UpperCase One</button>
+                            <button onClick={() => markAsDone(todo.id)}>Mark As Done</button>
                         </li>
                         )
                     })
