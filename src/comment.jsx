dc4e2162c1e323e713470a2f99982259acd660b3
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-06-19 10:13:25 +0530

    connecting comments from comments form

--- src/comment.jsx
+++ src/comment.jsx
@@ -1,23 +1,38 @@
 import { useState } from "react";
 import "./comment.css";
+import CommentsForm from "./commentsForm";
 
 export default function Comment() {
-    let [comments , setComments] = useState([{
-        username : "@shubhi123",
-        remarks : "nice work ! ",
-        rating : 4
-    }]);
-    return (
-        <div>
-            <h3>
-                All Comments
-            </h3>
-            <div className="comment">
-                <span>{comments[0].username}</span>
-                &nbsp;&nbsp;
-                <p>{comments[0].remarks}</p>
-                <span>{comments[0].rating}</span>
+  let [comments, setComments] = useState([
+    {
+      username: "@shubhi123",
+      remarks: "nice work ! ",
+      rating: 4,
+    },
+  ]);
+
+  let addNewComment = (comment) => {
+    setComments((currComments) => {
+      return [...currComments, comment];
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <h3>All Comments</h3>
+        {comments.map((comment, idx) => (
+            <div className="comment" key={idx}>
+              <span>{comment.username}</span>
+              &nbsp;&nbsp;
+              <p>{comment.remarks}</p>
+              <span>{comment.rating}</span>
             </div>
-        </div>
-    )
+        ))}
+      </div>
+      <hr />
+      <hr />
+      <CommentsForm addNewComment={addNewComment} />
+    </>
+  );
 }
